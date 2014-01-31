a17338e69777362f44aecf445205f9df66b97b0a
Ondrej Grover <ondrej.grover@gmail.com>
2014-01-31 10:24:23 +0100

    site_i18n: added safety check to run create_lang_copies only once and record_translations concept

--- site_i18n/site_i18n.py
+++ site_i18n/site_i18n.py
@@ -10,8 +10,18 @@ import six
 HIDE_UNTRANSLATED_POSTS = False
 
 
+# Global vars
+_translations = dict()
+_main_site_generated = False
+
+
 
 def create_lang_copies(pelican):
+    global _main_site_generated
+    if _main_site_generated:
+        return
+    else:
+        _main_site_generated = True
     for lang, config in I18N_CONFIGS.items():
         if not isinstance(config, six.types.ModuleType):
             try:
@@ -23,6 +33,14 @@ def create_lang_copies(pelican):
         pelican.run()
 
 
-        
+
+def record_translations(content_object):
+    global _translations
+    _translations[content_object] = content_object.translations
+    #content_object.translations = [] #TODO need to set them again somewhere to put them into templates and create translation links
+
+
+
 def register():
     signals.finalized.connect(create_lang_copies)
+    signals.content_object_init.connect(record_translations)
