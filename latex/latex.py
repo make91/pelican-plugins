e7626f98d2dfe808a08c30a415d68629ec20ec1a
Ondrej Grover <ondrej.grover@gmail.com>
2014-02-13 08:48:50 +0100

    latex: first concept of tex4ht based latex reader

--- latex/latex.py
+++ latex/latex.py
@@ -9,8 +9,79 @@ between `$..$` for inline math and `$$..$$` for displayed math. It also allows f
 writing equations in by using `\begin{equation}`...`\end{equation}`.
 """
 
-from pelican import signals
 
+import os
+from tempfile import mkdtemp, mkstemp
+from shutil import rmtree
+import subprocess as sp
+
+from pelican import signals
+from pelican.readers import HTMLReader
+
+
+class Tex4htReader(HTMLReader):
+    """Reads *.tex files (including LaTeX) using tex4ht
+
+    which transforms them into HTML files,
+    which are then read by the standard HTMLReader
+    """
+    enabled = True
+    file_extensions = ['tex']
+
+    def _make_cfg(self, temp_dir):
+        """Create a configuration file for tex4ht
+
+        this includes various metadata fields
+        """
+        fd , cfg_file = mkstemp(suffix='.cfg', dir=temp_dir)
+        f = os.fdopen(fd)
+        f.write("""\Preamble{html}
+        \def\tags#1{\gdef\@tags{#1}}
+        \def\category#1{\gdef\@category{#1}}
+        \def\summary#1{\gdef\@summary{#1}}
+        \def\modified#1{\gdef\@modified{#1}}
+        \Configure{TITLE+}{\@title}
+        \begin{document}
+        \HCode{<meta name="author" content="}\@author\HCode{" />}
+        \HCode{<meta name="author" content="}\@date\HCode{" />}
+        \HCode{<meta name="tags" content="}\@tags\HCode{" />}
+        \HCode{<meta name="category" content="}\@category\HCode{" />}
+        \HCode{<meta name="summary" content="}\@summary\HCode{" />}
+        \HCode{<meta name="summary" content="}\@modified\HCode{" />}
+        \EndPreamble
+        """)
+        f.close()
+        return cfg_file[:-4]                            # without .cfg
+                  
+    def read(self, filename):
+        """Let tex4ht create a HTML file and then parse it"""
+        temp_dir = mkdtemp()
+        cfg_file = self._make_cfg(temp_dir)
+        process = sp.Popen(['mk4ht', 'htlatex', filename,
+                            cfg_file + ',mathml,-css,NoFonts,charset=utf8',
+                            '-utf8',
+                            #'-d' + output_dir  # would only copy
+                            ],
+                            stdout=sp.DEVNULL,
+                            stderr=sp.DEVNULL,
+                            cwd=temp_dir,
+            )
+        process.wait()
+
+        html_output = os.path.join(temp_dir, filename.replace('.tex', '.html'))
+        content, metadata = super(Tex4htReader, self).read(html_output)  # parse the HTML
+        
+        # clean up
+        rmtree(temp_dir)
+
+        return content, metadata
+
+
+def add_reader(readers):
+    """Register the Tex4htReader"""
+    readers.reader_classes['tex'] = Tex4htReader
+
+    
 # Reference about dynamic loading of MathJax can be found at http://docs.mathjax.org/en/latest/dynamic.html
 # The https cdn address can be found at http://www.mathjax.org/resources/faqs/#problem-https
 latexScript = """
@@ -51,5 +122,6 @@ def register():
     """
         Plugin registration
     """
+    signals.readers_init.connect(add_reader)
     signals.article_generator_context.connect(addLatex)
     signals.page_generator_context.connect(addLatex)
