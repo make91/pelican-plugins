9953e1eef73bf163e623a8d963211dcff600e264
Ondrej Grover <ondrej.grover@gmail.com>
2014-02-21 19:15:18 +0100

    new plugin: latex_reader
    This plugin installs the TeXReader reader
    which can read most (La)TeX documents.

---
+++ latex_reader/README.rst
@@ -0,0 +1,84 @@
+==========================
+ LaTeX Reader For Pelican
+==========================
+
+This plugin installs the ``TeXReader`` reader class which can read most 
+(La)TeX files with the ``*.tex`` file extension. It is able to process
+almost any macro, e.g. `\input{}` or `\ref{}`.
+
+Requirements
+------------
+
+TeX4ht suite
+............
+
+The reader uses the `TeX4ht converter suite <http://www.tug.org/tex4ht/>`_
+to convert (La)TeX files to HTML. This suite uses a real (la)tex compiler
+under the hood, so it is best to install it as a part of a (La)TeX distribution
+like `TeX Live <https://www.tug.org/texlive/>`_ or `MiKTeX <http://miktex.org/>`_.
+
+The ``mk4ht`` command from that suite must be in ``PATH``.
+
+latex Pelican plugin (optional)
+...............................
+
+The reader renders math as `MathML <http://en.wikipedia.org/wiki/MathML>`_ by default.
+To render MathML more nicely and consistently accross most browsers, 
+the `latex Pelican plugin <https://github.com/getpelican/pelican-plugins/tree/master/latex>`_ 
+can be used to render it using the `MathJax <http://www.mathjax.org/>`_ JavaScript engine.
+
+Installation
+------------
+
+The plugin can be installed as described in the `Pelican plugin framework documentation <http://docs.getpelican.com/en/latest/plugins.html>`_.
+
+Usage
+-----
+
+Metadata in (La)TeX files
+.........................
+
+Arbitrary metadata can be specified in comments in this form, one per line
+
+.. code-block:: latex
+
+    %metadata tags: awesome,fun
+    %metadata lang: en
+    %metadata category: math
+
+Additionally, the information defined by the standard macros
+
+.. code-block:: latex
+
+    \title{}
+    \author{}
+    \date{}
+
+is also used as metadata.
+
+TeX4ht configuration
+....................
+
+You can configure arguments to the ``mk4ht`` command using config variables used
+in the ``TeXReader.__init__`` method. Alter them only if you know what you are doing!
+
+Known issues
+------------
+
+1. It is quite slow
+...................
+
+The reader is only as fast as the (la)tex compiler used under the hood.
+That is the price for having full (La)TeX support.
+
+2. HTML content contains a <style> tag and lots of CSS
+......................................................
+
+The TeX4ht suite relies on CSS to render content as close as possible
+to the (La)TeX output. The ``<style>`` tag has a `scoped
+<http://www.w3schools.com/tags/att_style_scoped.asp>`_ attribute, so
+in browsers that support it, only the desired content will be
+affected. However, it is unlikely that the unconventional class names
+used by TeX4ht will interfere with the CSS used by the theme. If you
+know a better solution, file an issue.
+
---
+++ latex_reader/latex_reader.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+"""==========================
+ LaTeX Reader For Pelican
+==========================
+
+This plugin installs the ``TeXReader`` reader class which can read most 
+(La)TeX files with the ``*.tex`` file extension. It is able to process
+almost any macro, e.g. `\input{}` or `\ref{}`.
+"""
+
+import os
+import re
+from tempfile import mkdtemp
+from shutil import rmtree
+import subprocess as sp
+
+from pelican import signals
+from pelican.readers import BaseReader
+from pelican.utils import pelican_open
+
+
+# configuration file for tex4ht that makes it output only body contents
+_DEFAULT_TEX4HT_CFG = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'pelican_tex4ht')
+# metadata fields regexp
+_METADATA_REGEXP = re.compile(r'%metadata (?P<key>.*): (?P<value>.*)$',
+                              re.UNICODE | re.IGNORECASE | re.MULTILINE)
+_META_MACROS_REGEXP = re.compile(r'\\(?P<key>author|date|title)\{(?P<value>.*?(?:\{.*\})*?)\}',
+                                 re.UNICODE | re.IGNORECASE | re.DOTALL)
+
+
+class TeXReader(BaseReader):
+    """Reads *.tex files (including LaTeX) using the tex4ht suite
+
+    which transforms them into HTML files (only the body contents are used)
+    """
+    enabled = True
+    file_extensions = ['tex']
+
+    def __init__(self, *args, **kwargs):
+        """Initialize reader and get compiler settings"""
+        super(TeXReader, self).__init__(*args, **kwargs)
+        self.compiler = self.settings.get('MK4HT_COMPILER', 'htlatex')
+        self.compiler_cfg = self.settings.get('MK4HT_COMPILER_CFG', _DEFAULT_TEX4HT_CFG)
+        self.compiler_opts = self.settings.get('MK4HT_COMPILER_OPTS', 'mathml')
+        self.tex4ht_opts = self.settings.get('TEX4HT_OPTS', ' -cunihtf -utf8') #notice the space
+        self.t4ht_opts = self.settings.get('T4HT_OPTS', '')
+
+    def compile_content(self, filename):
+        """Compile HTML body contents using mk4ht"""
+        temp_dir = mkdtemp()
+        try:
+            devnull = sp.DEVNULL          # NOQA
+        except AttributeError:                   # py2k
+            devnull = open(os.devnull, 'wb')
+        process = sp.Popen(['mk4ht', self.compiler, filename,
+                            self.compiler_cfg + ',' + self.compiler_opts,
+                            self.tex4ht_opts,
+                            self.t4ht_opts,
+                            ],
+                            stdout=devnull,
+                            stderr=devnull,
+                            cwd=temp_dir,
+            )
+        exit_code = process.wait()
+        output_template = os.path.join(temp_dir, os.path.basename(filename).replace('.tex', '{}'))
+        if exit_code != 0:
+            raise RuntimeError('mk4ht exited with code {},\nfull log: {}'.format(exit_code,
+                      output_template.format('.log'),
+                      ))
+
+        with pelican_open(output_template.format('.html')) as raw_content:
+            content = raw_content.strip() # remove extra whitespace
+        # prepend content with scoped css style info to render it properly
+        with pelican_open(output_template.format('.css')) as css_output:
+            content = '\n'.join([
+            # <code> to fool typogrify and make it not process the css stuff
+                '<div class="tex4ht-content">\n<style type="text/css" scoped>/*<code>*/',
+                css_output,
+                '/*</code>*/</style>',
+                content,
+                '</div>',
+                ])
+
+        # clean up
+        rmtree(temp_dir)
+
+        return content
+
+    def extract_metadata(self, filename):
+        """Extract metadata from comments and common metadata macros in source file"""
+        with pelican_open(filename) as source:
+            metadata = dict(match.groups() for match in _METADATA_REGEXP.finditer(source) if match is not None)
+            metadata.update(match.groups() for match in _META_MACROS_REGEXP.finditer(source) if match is not None)
+            for key, value in metadata.items():
+                metadata[key] = self.process_metadata(key, value)
+
+        return metadata
+
+    def read(self, filename):
+        """Let tex4ht create a HTML body contents and parse the *.tex file for metadata"""
+        return self.compile_content(filename), self.extract_metadata(filename)
+
+
+def add_reader(readers):
+    """Register the TeXReader"""
+    readers.reader_classes['tex'] = TeXReader
+
+
+def register():
+    """Plugin registration"""
+    signals.readers_init.connect(add_reader)
